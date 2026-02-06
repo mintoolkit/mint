4137d20725f3802527857cd512412fb373f07ef8
Ernst von Oelsen <ernst.vonoelsen@tngtech.com>
2026-02-06 13:08:39 +0100

    Fix failing end-2-end tests.

--- scripts/src.build.sh
+++ scripts/src.build.sh
@@ -23,10 +23,11 @@ LD_FLAGS="-s -w -X github.com/mintoolkit/mint/pkg/version.appVersionTag=${TAG} -
 go generate github.com/mintoolkit/mint/pkg/appbom
 
 pushd ${BDIR}/cmd/mint
-GOOS=linux GOARCH=amd64 go build -mod=vendor -trimpath -ldflags="${LD_FLAGS}" -a -tags 'remote netgo osusergo containers_image_openpgp containers_image_docker_daemon_stub containers_image_fulcio_stub containers_image_rekor_stub' -o "${BDIR}/bin/linux/mint" 
+GOOS=linux GOARCH=amd64 go build -mod=vendor -trimpath -ldflags="${LD_FLAGS}" -a -tags 'remote netgo osusergo containers_image_openpgp containers_image_docker_daemon_stub containers_image_fulcio_stub containers_image_rekor_stub' -o "${BDIR}/bin/linux/mint"
 GOOS=darwin GOARCH=amd64 go build -mod=vendor -trimpath -ldflags="${LD_FLAGS}" -a -tags 'remote netgo osusergo containers_image_openpgp containers_image_docker_daemon_stub containers_image_fulcio_stub containers_image_rekor_stub' -o "${BDIR}/bin/mac/mint"
 GOOS=linux GOARCH=arm go build -mod=vendor -trimpath -ldflags="${LD_FLAGS}" -a -tags 'remote netgo osusergo containers_image_openpgp containers_image_docker_daemon_stub containers_image_fulcio_stub containers_image_rekor_stub' -o "$BDIR/bin/linux_arm/mint"
 GOOS=linux GOARCH=arm64 go build -mod=vendor -trimpath -ldflags="${LD_FLAGS}" -a -tags 'remote netgo osusergo containers_image_openpgp containers_image_docker_daemon_stub containers_image_fulcio_stub containers_image_rekor_stub' -o "$BDIR/bin/linux_arm64/mint"
+GOOS=linux GOARCH=amd64 go build -mod=vendor -trimpath -ldflags="${LD_FLAGS}" -a -tags 'remote netgo osusergo containers_image_openpgp containers_image_fulcio_stub containers_image_rekor_stub' -o "${BDIR}/bin/linux/mint-e2e"
 popd
 
 pushd ${BDIR}/cmd/mint-sensor
@@ -55,7 +56,7 @@ popd
 rm -rfv ${BDIR}/dist_linux
 mkdir ${BDIR}/dist_linux
 cp ${BDIR}/bin/linux/mint ${BDIR}/dist_linux/mint
-cp ${BDIR}/bin/linux/mint-sensor ${BDIR}/dist_linux/mint-sensor
+cp ${BDIR}/bin/linux/mint-e2e ${BDIR}/dist_linux/mint-sensor
 pushd ${BDIR}/dist_linux
 ln -s mint docker-slim
 ln -s mint slim
